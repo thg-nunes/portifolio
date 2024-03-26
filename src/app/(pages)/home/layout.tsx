291e03e1b7dfa43d8601b9beb5f2360e6243c6a2
thiago nunes <desenvolvedor.nunes@gmail.com>
2024-03-26 14:20:53 -0300

    refactor: move footer para layout global e cria layout.tsx para a pagina Home

---
+++ src/app/(pages)/home/layout.tsx
@@ -0,0 +1,31 @@
+import { LinkComponent } from '../../components/liink'
+
+import { appRoutes } from '@/src/utils/appRouts'
+
+export const metadata = {
+  title: 'Home',
+  description:
+    'Aqui você vai encontrar um breve resumo de quem sou e oo que faço, além de trabalhos já realizados.',
+}
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <header>
+        <nav className="border-b border-gray-700 text-white">
+          <section className="relative mx-auto flex max-w-7xl justify-between px-4 py-1">
+            <span className="text-xl font-bold leading-10">{`<thg />`}</span>
+            <ul className="flex flex-row-reverse items-center gap-4 md:flex-row">
+              {appRoutes.map(({ elementId, linkText }) => (
+                <li className="cursor-pointer px-3 text-center font-semibold leading-10 duration-300 hover:bg-gray-450 hover:text-white dark:text-white md:hover:bg-gray-700">
+                  <LinkComponent elementId={elementId} linkText={linkText} />
+                </li>
+              ))}
+            </ul>
+          </section>
+        </nav>
+      </header>
+      {children}
+    </>
+  )
+}
--- src/app/(pages)/home/page.tsx
+++ src/app/(pages)/home/page.tsx
@@ -1,16 +1,12 @@
 import Image from 'next/image'
-import { AiOutlineMail } from 'react-icons/ai'
-import { GithubLogo, LinkedinLogo, HandWaving } from '@phosphor-icons/react/dist/ssr'
+import { HandWaving } from '@phosphor-icons/react/dist/ssr'
 
 import { Job } from '../../components/job'
 import { GoTop } from '../../components/goTop'
-import { Media } from '../../components/media'
 import { Skills } from '../../components/skills'
 import { Experiences } from '../../components/experiences'
 
 export default function Home() {
-  const year = new Date().getFullYear()
-
   return (
     <main>
       <div className="mx-auto flex max-w-7xl flex-col-reverse items-center justify-between px-4 py-10 text-white md:flex-row">
@@ -133,35 +129,6 @@ export default function Home() {
 
         <Skills />
       </div>
-
-      <footer
-        className="flex flex-col items-center space-y-6 px-4 py-16 text-white md:px-0"
-        id="contato"
-      >
-        <section className="mx-auto flex max-w-7xl items-center justify-center gap-3">
-          <div className="flex items-center gap-2">
-            <AiOutlineMail className="h-7 w-7" />
-            desenvolvedor.nunes@gmail.com
-          </div>
-
-          <section className="flex gap-3">
-            <Media
-              target="_blank"
-              description="github"
-              href="https://github.com/thg-nunes"
-              icon={<GithubLogo size={24} weight="bold" />}
-            />
-            <Media
-              target="_blank"
-              description="linkedin"
-              href="https://www.linkedin.com/in/thiago-nunes-3a7771219/"
-              icon={<LinkedinLogo size={24} weight="bold" />}
-            />
-          </section>
-        </section>
-        <p>Feito por: Thiago Nunes</p>
-        <span>Copyright ©{year} All rights reserved </span>
-      </footer>
       <GoTop />
     </main>
   )
